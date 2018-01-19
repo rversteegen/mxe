24089e5385331e0a51033a5c939fa5f8d90ffa66
Ralph Versteegen <teeemcee@gmail.com>
2018-01-20 02:12:19 +1300

    Merge sdl_mixer.mk changes into sdl_mixer_ohrrpgce.mk, and provide missing instructions

--- src/sdl_mixer_ohrrpgce.mk
+++ src/sdl_mixer_ohrrpgce.mk
@@ -1,7 +1,16 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 # Variant on sdl_mixer.mk
+# Unfortunately not entirely automated:
+# First ensure mxe has compiled sdl, eg run the 'make' command given below.
+# Check usr/i686-w64-mingw32.static/lib/libSDL.la contains dlname='../bin/SDL.dll'
+# and place a copy of SDL.dll in usr/i686-w64-mingw32.static/bin
 # Compile with "make MXE_TARGETS=i686-w64-mingw32.static sdl_mixer_ohrrpgce"
+# Strip the resulting .dll:
+#  cp usr/i686-w64-mingw32.static/bin/SDL_mixer.dll SDL_mixer.dll
+#  strip SDL_mixer.dll
+# Check that it links dynamically to SDL.dll:
+#  objdump -x SDL_mixer.dll | grep SDL.dll
 
 # FLAC disabled because we don't use it.
 # libmad used instead of smpeg because it's smaller and doesn't crash with
@@ -10,16 +19,17 @@
 # https://www.slimesalad.com/forum/viewtopic.php?t=6810
 
 PKG             := sdl_mixer_ohrrpgce
+$(PKG)_WEBSITE  := https://www.libsdl.org/projects/SDL_mixer/
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.2.12
 $(PKG)_CHECKSUM := 1644308279a975799049e4826af2cfc787cad2abb11aa14562e402521f86992a
 $(PKG)_SUBDIR   := SDL_mixer-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL_mixer-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://www.libsdl.org/projects/SDL_mixer/release/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc libmodplug ogg sdl libmad vorbis  #libmikmod
+$(PKG)_URL      := https://www.libsdl.org/projects/SDL_mixer/release/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc libmodplug ogg sdl libmad vorbis  #libmikmod
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://hg.libsdl.org/SDL_mixer/tags' | \
+    $(WGET) -q -O- 'https://hg.libsdl.org/SDL_mixer/tags' | \
     $(SED) -n 's,.*release-\([0-9][^<]*\).*,\1,p' | \
     grep '^1\.' | \
     $(SORT) -V | \
@@ -27,15 +37,15 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    $(SED) -i 's,^\(Requires:.*\),\1 vorbisfile,' '$(1)/SDL_mixer.pc.in'
+    $(SED) -i 's,^\(Requires:.*\),\1 vorbisfile,' '$(SOURCE_DIR)/SDL_mixer.pc.in'
     echo \
         'Libs.private:' \
-        >> '$(1)/SDL_mixer.pc.in'
-    $(SED) -i 's,for path in /usr/local; do,for path in; do,' '$(1)/configure'
+        >> '$(SOURCE_DIR)/SDL_mixer.pc.in'
+    $(SED) -i 's,for path in /usr/local; do,for path in; do,' '$(SOURCE_DIR)/configure'
 
-    cd '$(1)' && ./configure \
-        --host='$(TARGET)' \
-        --prefix='$(PREFIX)/$(TARGET)' \
+    cd '$(BUILD_DIR)' && $(SOURCE_DIR)/configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --enable-shared \
         --with-sdl-prefix='$(PREFIX)/$(TARGET)' \
         --disable-sdltest \
         --disable-music-mod \
@@ -44,11 +54,15 @@ define $(PKG)_BUILD
         --disable-music-flac \
         --disable-music-mp3 \
 	--enable-music-mp3-mad-gpl \
-        --disable-music-mod-shared \
-        --disable-music-ogg-shared \
-        --disable-music-flac-shared \
-        --disable-music-mp3-shared \
-        --disable-smpegtest \
+        $(if $(BUILD_STATIC), \
+            --disable-music-mod-shared \
+            --disable-music-ogg-shared \
+            --disable-music-flac-shared \
+            --disable-music-mp3-shared \
+            --disable-smpegtest \
+            --with-smpeg-prefix='$(PREFIX)/$(TARGET)' \
+            , \
+            --without-smpeg) \
 	PKG_CONFIG='$(TARGET)-pkg-config' \
 	LIBMIKMOD_CONFIG='$(PREFIX)/$(TARGET)/bin/libmikmod-config' \
         WINDRES='$(TARGET)-windres' \
@@ -59,7 +73,8 @@ define $(PKG)_BUILD
     # in errors like -lpulse not found.
     # LDFLAGS actually get passed to libtool which passes them to gcc.
 
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' build/libSDL_mixer.la
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install-hdrs install-lib
 
     '$(TARGET)-gcc' \
         -W -Wall -Werror -ansi -pedantic \
